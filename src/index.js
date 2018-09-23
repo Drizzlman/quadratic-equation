94d7811520dca0d154dada5db95154a7f20ee58c
Drizzlman <kovalko.alexey@yandex.by>
2018-09-23 11:28:45 +0300

    Task_4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var Array = equation.split(' ');
+    var VariableA = Array[0];
+    var VariableB = null;
+    var VariableC = null;
+    var Discriminant = null;
+    var FirstAnswer = null;
+    var SecondAnswer = null;
+    var Answear = null;
+    VariableB = parseInt(Array[3] + Array[4]);
+    VariableC = parseInt(Array[7] + Array[8]);
+
+    Discriminant = (Math.pow(VariableB, 2) - (4 * VariableA * VariableC));
+
+    FirstAnswer = Math.round(((-1 * VariableB) + Math.sqrt(Discriminant)) / (2 * VariableA));
+    SecondAnswer = Math.round(((-1 * VariableB) - Math.sqrt(Discriminant)) / (2 * VariableA));
+
+    Answear = [FirstAnswer, SecondAnswer];
+
+    Answear.sort((VariableA, VariableB) => VariableA - VariableB);
+
+    return Answear;
+
 }
+
